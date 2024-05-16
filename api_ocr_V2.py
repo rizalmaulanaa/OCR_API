ec73171c2f369db967005387ca2e0749837e656f
Rizal Maulana <rizal.0951@gmail.com>
2024-05-16 08:51:22 +0700

    add json input

--- api_ocr_V2.py
+++ api_ocr_V2.py
@@ -1,11 +1,13 @@
 import re
 import sys
+import json
 import uvicorn
 import importlib
 
 from glob import glob
-from fastapi import FastAPI, File, UploadFile
+from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile, Form
 
 sys.path.append('..')
 
@@ -20,85 +22,9 @@ from OCR_API.text_segmentation_V2 import model_segmentation
 
 def prediction_seg(image,
                    file_name,
-                   model_name='YOLO_n_80', 
+                   model_name={'YOLO_n_80': True}, 
                    smoothing=True, 
                    sm_kernel=(3,3)):
-
-    # Get status
-    flag_inverted = get_status_image(image)
-    
-    # Inverted Image
-    image = preprocessing_image(image, 
-                                inverted=flag_inverted, 
-                                normalize=False, 
-                                smoothing=smoothing, 
-                                sm_kernel=sm_kernel)
-
-    # Initialize the model segmentation
-    md = model_segmentation(model_name, preprocessing=False, smoothing=smoothing, sm_kernel=sm_kernel)
-
-    # Prediction
-    y_pred, bbx_list, bbx_n_list, conf_list, _, _ = md.prediction(image)
-
-    # Create the json format 
-    file_names = file_name.split('/')[-1]
-    pred = re.findall(r'[0-9A-zA-Z]', y_pred)
-    img_shape = list(image.shape)
-
-    data_json = [{
-        'name': file_names,
-        'prediction': pred,
-        'conf': conf_list,
-        'bbx': bbx_list,
-        'bbxN': bbx_n_list,
-        'imageShape': img_shape
-    }]
-    return data_json
-
-def prediction_recog(image,
-                     file_name,
-                     model_name='YOLO_m_60', 
-                     smoothing=True, 
-                     sm_kernel=(3,3)):
-
-    # Inverted Image
-    type_image_input = model_name.split('-')
-
-    if len(type_image_input) > 1:
-        # Get status
-        flag_inverted = get_status_image(image)
-
-        image = preprocessing_image(image, 
-                                    inverted=flag_inverted, 
-                                    normalize=False, 
-                                    smoothing=smoothing, 
-                                    sm_kernel=sm_kernel)
-
-    # Initialize the model recognition
-    md_recog = model_recognition(model_name=model_name, preprocessing=False, smoothing=True, sm_kernel=sm_kernel)
-
-    y_pred, conf_list, bbx_list, bbx_n_list = md_recog.prediction(image)
-    
-    # Create the json format 
-    file_names = file_name.split('/')[-1]
-    pred = re.findall(r'[0-9A-zA-Z]', y_pred)
-    img_shape = list(image.shape)
-
-    data_json = [{
-        'name': file_names,
-        'prediction': pred,
-        'conf': conf_list,
-        'bbx': bbx_list,
-        'bbxN': bbx_n_list,
-        'imageShape': img_shape
-    }]
-    return data_json
-
-def prediction_recog_double(image,
-                            file_name,
-                            model_name={'YOLO_m_60': False, 'YOLO_n_100-plate2': True}, 
-                            smoothing=True, 
-                            sm_kernel=(3,3)):
 
     data_json_list = []
 
@@ -107,7 +33,50 @@ def prediction_recog_double(image,
         type_image_input = md_name.split('-')
 
         # Get status
-        print(image.shape)
+        flag_inverted = get_status_image(image, flag=v)
+        image = preprocessing_image(image, 
+                                    inverted=flag_inverted, 
+                                    normalize=False, 
+                                    smoothing=smoothing, 
+                                    sm_kernel=sm_kernel)
+
+        # Initialize the model segmentation
+        md = model_segmentation(model_name, preprocessing=False, smoothing=smoothing, sm_kernel=sm_kernel)
+
+        # Prediction
+        y_pred, bbx_list, bbx_n_list, conf_list, _, _ = md.prediction(image)
+
+        # Create the json format 
+        file_names = file_name.split('/')[-1]
+        pred = re.findall(r'[0-9A-zA-Z]', y_pred)
+        img_shape = list(image.shape)
+
+        data_json = {
+            'name': file_names,
+            'prediction': pred,
+            'conf': conf_list,
+            'bbx': bbx_list,
+            'bbxN': bbx_n_list,
+            'imageShape': img_shape
+        }
+
+        data_json_list.append(data_json)
+
+    return data_json_list
+
+def prediction_recog(image,
+                     file_name,
+                     model_name={'YOLO_m_60': False, 'YOLO_n_100-plate2': True}, 
+                     smoothing=True, 
+                     sm_kernel=(3,3)):
+
+    data_json_list = []
+
+    for md_name, v in model_name.items():
+        # Inverted Image
+        type_image_input = md_name.split('-')
+
+        # Get status
         flag_inverted = get_status_image(image, flag=v)
         image = preprocessing_image(image, 
                                     inverted=flag_inverted, 
@@ -192,31 +161,23 @@ app.add_middleware(
 
 # Text segmentation endpoint
 @app.post("/segmentation")
-async def segmentation_async(image: UploadFile = File(...)):
+async def segmentation_async(image: UploadFile = File(...), json_data: Optional[str] = Form(None)):
     file_name = image.filename
     image = load_image_into_numpy_array(await image.read())
-    json_file = prediction_seg(image, file_name)
+    md_dict = json.loads(json_data)
+
+    json_file = prediction_seg(image, file_name, model_name=md_dict)
     
     return json_file
 
+# Text recognition endpoint
 @app.post("/recognition")
-async def recognition_async(image: UploadFile = File(...)):
+async def recognition_async(image: UploadFile = File(...), json_data: Optional[str] = Form(None)):
     file_name = image.filename
     image = load_image_into_numpy_array(await image.read())
-    json_file = prediction_recog(image, file_name)
-    
-    return json_file
+    md_dict = json.loads(json_data)
 
-@app.post("/recognition_double")
-async def recognition_double_async(image: UploadFile = File(...)):
-    file_name = image.filename
-    image = load_image_into_numpy_array(await image.read())
-    md_dict = {
-        'YOLO_m_60': False, 
-        'YOLO_n_100-plate2': True
-    }
-
-    json_file = prediction_recog_double(image, file_name, model_name=md_dict)
+    json_file = prediction_recog(image, file_name, model_name=md_dict)
     
     return json_file
 
