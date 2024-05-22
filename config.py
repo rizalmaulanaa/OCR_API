31051e76188169f018ef94ff735c8baa53f39853
Rizal Maulana <rizal.0951@gmail.com>
2024-05-22 08:31:07 +0700

    update code

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
+path = (Path(__file__)).resolve()
+path = '/'.join(str(path).split('/')[:-1])
+path += '/pretained_model/'
+
 config_dict = {
-    'path_pretrain': '/Users/rizalmaulana/Desktop/Icon_Plus/pretrained_model/',
-    'path_saved': '/Users/rizalmaulana/Desktop/Icon_Plus/data_save/'
+    'path_pretrain': path
 }
